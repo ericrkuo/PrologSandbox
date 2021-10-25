cd98605d4a586fd753e043353d6b9c8a7f7288e1
ericrkuo <ericrkuo@gmail.com>
2021-10-24 23:25:13 -0700

    LPN Chapter 1 Notes - http://www.let.rug.nl/bos/lpn/lpnpage.php?pagetype=html&pageid=lpn-htmlch1 - Simple KB's, basic syntax

---
+++ LPN_1.pl
@@ -0,0 +1,87 @@
+:- discontiguous playsAirGuitar/1, happy/1, listens2Music/1, woman/1.
+
+/*
+KB1
+
+- a knowledge base if a collection of facts (KB1 has 5 facts), used to state things are unconditionally true
+- mia, jody, yolanda are atoms
+- woman, playsAirGuitar are predicates (functors with an arity)
+- woman(yolanda) is a complex term (structur)
+
+- Queries
+?- woman(mia) - asking if Mia is a woman
+?- woman(asdasd) - false since it is not a fact in KB1
+?- man(mia) - false since Prolog has no information about the predicate
+*/
+woman(mia).
+woman(jody).
+woman(yolanda).
+playsAirGuitar(jody).
+party.
+
+/*
+KB2
+
+Rule
+- head :- body
+- if KB contains a rule, and Prolog knows body follows from KB, then Prolog will infer haed
+*/
+happy(yolanda).
+listens2Music(mia).
+listens2Music(yolanda):-  happy(yolanda).
+playsAirGuitar(mia):-  listens2Music(mia).
+playsAirGuitar(yolanda):-  listens2Music(yolanda).
+
+/*
+KB3
+
+- can have rules with more than one items in the body 
+*/
+happy(vincent).
+listens2Music(butch).
+playsAirGuitar(vincent):- listens2Music(vincent), happy(vincent).
+playsAirGuitar(butch):- happy(butch).
+playsAirGuitar(butch):- listens2Music(butch).
+% OR can use disjunction playsAirGuitar(butch) :- happy(butch); listesns2Music(butch).
+
+/*
+KB4
+
+- queries using variables
+?- woman(X).
+?- loves(X, mia).
+?- loves(X,Y)
+*/
+woman(mia).
+woman(jody).
+woman(yolanda).
+
+loves(vincent,mia).
+loves(marsellus,mia).
+loves(pumpkin,honey_bunny).
+loves(honey_bunny,pumpkin).
+
+/*
+KB5
+
+- using variables in program
+- this is defining a concept about jealousy
+*/
+loves(vincent,mia).
+loves(marsellus,mia).
+loves(pumpkin,honey_bunny).
+loves(honey_bunny,pumpkin).
+
+jealous(X,Y):-  loves(X,Z),  loves(Y,Z).
+
+/*
+Nested Complex terms (recursive structure)
+- two arguments (arity = 2)
+- one is a variable
+- the other is a complex term father(father(father(butch)))
+*/
+hide(X,father(father(father(butch)))).
+
+% Predicates need to be identified by functor and number of arguments (arity)
+loves(X,Y,Z). % loves/3
+loves(W,X,Y,Z). % loves/4
